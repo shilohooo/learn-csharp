b0bad5fd03d7f8aa83b79afcd4aa39dd6a1c194c
shiloh <lixiaolei595@gmail.com>
2023-12-13 12:11:05 +0800

    refactor: generate sql script

---
+++ ef-core/EFCoreQuickStart/script.sql
@@ -0,0 +1,40 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Grades] (
+    [GradeId] int NOT NULL IDENTITY,
+    [GradeName] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Grades] PRIMARY KEY ([GradeId])
+);
+GO
+
+CREATE TABLE [Students] (
+    [StudentId] int NOT NULL IDENTITY,
+    [FirstName] nvarchar(max) NOT NULL,
+    [LastName] nvarchar(max) NOT NULL,
+    [GradeId] int NOT NULL,
+    CONSTRAINT [PK_Students] PRIMARY KEY ([StudentId]),
+    CONSTRAINT [FK_Students_Grades_GradeId] FOREIGN KEY ([GradeId]) REFERENCES [Grades] ([GradeId]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_Students_GradeId] ON [Students] ([GradeId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20231212130423_InitialSchoolDB', N'8.0.0');
+GO
+
+COMMIT;
+GO
+

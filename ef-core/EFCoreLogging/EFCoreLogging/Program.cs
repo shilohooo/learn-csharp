446cf8f1e7407cc332d975f41d519bbd916336f8
shiloh <lixiaolei595@gmail.com>
2023-12-18 17:58:54 +0800

    feat: ef core logging config example

---
+++ ef-core/EFCoreLogging/EFCoreLogging/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace EFCoreLogging;
+
+/// <summary>
+/// EF Core 日志配置
+/// </summary>
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using var context = new SchoolContext();
+        // 新增
+        // var student = new Student { FirstName = "Bruce", LastName = "Lee" };
+        // context.Add(student);
+        // context.SaveChanges();
+
+        // 查询所有学生信息
+        // var students = context.Students.ToList();
+        // foreach (var item in students)
+        // {
+        //     Console.WriteLine(item);
+        // }
+
+        // 根据 ID 查询学生信息
+        const int id = 1;
+        var student = context.Students.FirstOrDefault(s => id == s.Id);
+        Console.WriteLine(student);
+
+        if (student is null)
+        {
+            return;
+        }
+
+        // 修改学生信息
+        // student.LastName = "Tom";
+        // context.Update(student);
+        // context.SaveChanges();
+
+        // 删除学生信息
+        context.Remove(student);
+        context.SaveChanges();
+    }
+}
---
+++ ef-core/EFCoreLogging/EFCoreLogging/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EFCoreLogging;
+
+/// <summary>
+/// 数据上下文
+/// </summary>
+public class SchoolContext : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        const string connStr = @"Server=(localdb)\MSSQLLocalDB;Database=EFCoreLoggingDemo;Trusted_Connection=True;";
+        optionsBuilder.UseSqlServer(connStr)
+            // 日志输出到控制台，并设置日志级别为 Information
+            .LogTo(Console.WriteLine, LogLevel.Information)
+            // 日志记录敏感数据，建议只在开发环境启动
+            .EnableSensitiveDataLogging()
+            // 日志记录更详细的错误信息
+            .EnableDetailedErrors();
+    }
+}

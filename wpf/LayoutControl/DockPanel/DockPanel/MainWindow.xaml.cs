91d259428004bd30c5ffefc6889accc45a416eb6
shiloh <lixiaolei595@gmail.com>
2023-12-21 16:42:55 +0800

    feat: dock panel layout example

---
+++ wpf/LayoutControl/DockPanel/DockPanel/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace DockPanel;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}

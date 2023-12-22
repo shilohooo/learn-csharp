1766ae7ebc02d6ea87075f62a9223f6e58abbd43
shiloh <lixiaolei595@gmail.com>
2023-12-22 15:10:58 +0800

    feat(ContentControl): ToolTip example

---
+++ wpf/ContentControl/ToolTip/ToolTip/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace ToolTip;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ToWpfHomePage(object sender, RoutedEventArgs e)
+    {
+        Process.Start("explorer.exe", "https://learn.microsoft.com/en-us/dotnet/desktop/wpf/overview");
+    }
+}

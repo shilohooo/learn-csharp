2b0a26b8b5fff33b860344c3a97bb1fa2aa93c48
shiloh <lixiaolei595@gmail.com>
2023-12-22 10:30:31 +0800

    feat(ContentControl): RepeatButton example

---
+++ wpf/ContentControl/RepeatButton/RepeatButton/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace RepeatButton;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private int _Counter = 0;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnClick(object sender, RoutedEventArgs e)
+    {
+        _Counter++;
+        Console.WriteLine($"当前时间：{DateTime.Now} {DateTime.Now.Millisecond}，重复执行几次：{_Counter}");
+    }
+}

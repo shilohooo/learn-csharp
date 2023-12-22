8d5fe048bb57bb5a696bd3b6787388f332c98dd1
shiloh <lixiaolei595@gmail.com>
2023-12-22 10:39:21 +0800

    feat(ContentControl): Label example

---
+++ wpf/ContentControl/Label/Label/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Label;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// 弹窗显示当前时间
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void SayHi(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show($"你好，现在是北京时间：{DateTime.Now}");
+    }
+}

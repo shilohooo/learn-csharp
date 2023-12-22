146ef17aade1c7f409e866fbbb5b863aa303796d
shiloh <lixiaolei595@gmail.com>
2023-12-22 11:30:47 +0800

    feat(ContentControl): TextBox example

---
+++ wpf/ContentControl/TextBox/TextBox/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace TextBox;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Confirm(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show($"您输入的用户名为：{TextBox.Text}");
+    }
+}

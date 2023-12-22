176655d6d0d4413e985c4e3ce8a343dd610abcbe
shiloh <lixiaolei595@gmail.com>
2023-12-22 16:49:59 +0800

    feat(ContentControl): Image example

---
+++ wpf/ContentControl/Image/Image/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Image;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        var path = Environment.CurrentDirectory + "\\" + "huaji.jpg";
+        var imgSource = BitmapFrame.Create(new Uri(path), BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+        LocalImage.Source = imgSource;
+    }
+}

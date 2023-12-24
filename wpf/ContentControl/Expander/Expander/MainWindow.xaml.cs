e0db9c99cb7ede8b964aa9bacee9314c423b6d21
shiloh <lixiaolei595@gmail.com>
2023-12-24 16:52:17 +0800

    feat(ContentControl): Expander example

---
+++ wpf/ContentControl/Expander/Expander/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Expander;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnExpanded(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Expander控件展开");
+    }
+
+    private void OnCollapsed(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("Expander控件折叠");
+    }
+}

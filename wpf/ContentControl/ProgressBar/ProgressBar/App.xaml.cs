06623bbf1d63dbd6ffa3970fe0c40bdea2a8447f
shiloh <lixiaolei595@gmail.com>
2023-12-24 16:27:39 +0800

    feat(ContentControl): ProgressBar example

---
+++ wpf/ContentControl/ProgressBar/ProgressBar/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace ProgressBar;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ wpf/ContentControl/ProgressBar/ProgressBar/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace ProgressBar;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        Loaded += (_, _) =>
+        {
+            Task.Factory.StartNew(() =>
+            {
+                for (var i = 0; i <= 100; i++)
+                {
+                    var progress = i;
+                    Dispatcher.Invoke(() =>
+                    {
+                        TextBlock.Text = $"{progress}%";
+                        ProgressBar.Value = progress;
+                    });
+
+                    Task.Delay(100).Wait();
+                }
+            });
+        };
+    }
+}

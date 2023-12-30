f13b29a9cbdc212eddf687371cff21d9b7a1a885
shiloh <lixiaolei595@gmail.com>
2023-12-30 19:30:46 +0800

    feat(CollectionControl): TabControl example

---
+++ wpf/CollectionControl/TabControl/TabControl/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace TabControl;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ wpf/CollectionControl/TabControl/TabControl/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TabControl;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// 获取当前选中的标签页项 TabItem
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var tabControl = sender as System.Windows.Controls.TabControl;
+        var selectedItem = tabControl?.SelectedItem as TabItem;
+        var selectedContent = tabControl?.SelectedContent;
+        TextBlock.Text = $"标题：{selectedItem?.Header}, 内容：{selectedContent}";
+    }
+}

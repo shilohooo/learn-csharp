b5dc27e514a71c41a0a9d29c10f9c97fabab0e94
shiloh <lixiaolei595@gmail.com>
2023-12-30 21:31:37 +0800

    feat(CollectionControl): ContextMenu example

---
+++ wpf/CollectionControl/ContextMenu/ContextMenu/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ContextMenu;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnMenuItemClick(object sender, RoutedEventArgs e)
+    {
+        var menuItem = sender as MenuItem;
+        MessageBox.Show(menuItem?.Header.ToString());
+    }
+}

e94daf5ad5a810233cd56b33aa947aaaec03e968
shiloh <lixiaolei595@gmail.com>
2023-12-31 12:19:53 +0800

    feat(ShapeControl): Ellipse example

---
+++ wpf/ShapeControl/Ellipse/Ellipse/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Ellipse;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}

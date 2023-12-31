2c79c4dfbf3f113f0f31b9698a602c096bc68ecb
shiloh <lixiaolei595@gmail.com>
2023-12-31 12:47:51 +0800

    feat(ShapeControl): Line example

---
+++ wpf/ShapeControl/Line/Line/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+
+namespace Line;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Loaded += (_, _) =>
+        {
+            var number = 10;
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (number == 1)
+                    {
+                        number = 10;
+                    }
+
+                    var copy = number;
+                    var action = new Action(() => { FlowLine.StrokeDashOffset = copy; });
+                    Application.Current.Dispatcher.BeginInvoke(action);
+                    number--;
+                    Thread.Sleep(250);
+                }
+            });
+        };
+    }
+}

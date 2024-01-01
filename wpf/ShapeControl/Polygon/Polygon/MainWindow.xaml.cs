28ad2375c0a4cc97902831f073e53a0f487e3e13
shiloh <lixiaolei595@gmail.com>
2024-01-01 12:23:10 +0800

    feat(ShapeControl): Polygon example

---
+++ wpf/ShapeControl/Polygon/Polygon/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Polygon;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private int _count;
+    private System.Windows.Shapes.Polygon? _polygon;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        if (_count++ == 0)
+        {
+            _polygon = new System.Windows.Shapes.Polygon
+            {
+                StrokeThickness = 5,
+                Stroke = Brushes.Red
+            };
+            Canvas.Children.Add(_polygon);
+        }
+
+        var point = e.GetPosition(Canvas);
+        _polygon?.Points.Add(point);
+    }
+
+    private void OnPreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        _count = 0;
+    }
+}

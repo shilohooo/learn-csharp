bebca8b0ebd05d9e0980f388c6a813c120d308cf
shiloh <lixiaolei595@gmail.com>
2024-01-04 21:34:52 +0800

    feat(Style): Resource example

---
+++ wpf/Style/Style.Resource/Style.Resource/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Style.Resource;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        // 设置应用启动之后要打开的窗口或者页面
+        StartupUri = new Uri("MainWindow.xaml", UriKind.Relative);
+    }
+}

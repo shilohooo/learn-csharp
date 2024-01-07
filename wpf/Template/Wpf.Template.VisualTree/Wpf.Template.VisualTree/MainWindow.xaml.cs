97f0b4ac874c868e2a1e5497dd2124d4e6c16553
shiloh <lixiaolei595@gmail.com>
2024-01-07 22:23:40 +0800

    feat(Template): VisualTree example

---
+++ wpf/Template/Wpf.Template.VisualTree/Wpf.Template.VisualTree/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Wpf.Template.VisualTree;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ShowVisualTree(object sender, RoutedEventArgs e)
+    {
+        var rootNode = new TreeViewItem { Header = "VisualTreeRootNode" };
+
+        LoadVisualTree(rootNode, this);
+
+        VisualTreeView.Items.Add(rootNode);
+    }
+
+    private static void LoadVisualTree(ItemsControl node, object element)
+    {
+        if (element is not DependencyObject dependencyObject)
+        {
+            return;
+        }
+
+        var currentNode = new TreeViewItem { Header = element.GetType().Name };
+        node.Items.Add(currentNode);
+
+        var childrenCount = VisualTreeHelper.GetChildrenCount(dependencyObject);
+        for (var i = 0; i < childrenCount; i++)
+        {
+            LoadVisualTree(
+                currentNode,
+                VisualTreeHelper.GetChild(dependencyObject, i)
+            );
+        }
+    }
+}

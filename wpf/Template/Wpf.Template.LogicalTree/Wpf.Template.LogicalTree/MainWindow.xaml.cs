6e9cf806c704b1c5d18588fdd976c28b231a2db0
shiloh <lixiaolei595@gmail.com>
2024-01-07 22:13:52 +0800

    feat(Template): LogicalTree example

---
+++ wpf/Template/Wpf.Template.LogicalTree/Wpf.Template.LogicalTree/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Wpf.Template.LogicalTree;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ShowLogicalTree(object sender, RoutedEventArgs e)
+    {
+        var rootNode = new TreeViewItem { Header = "Root" };
+
+        LoadLogicTree(rootNode, this);
+
+        LogicalTreeView.Items.Add(rootNode);
+    }
+
+    private static void LoadLogicTree(ItemsControl treeViewItem, object element)
+    {
+        if (element is not DependencyObject dependencyObject)
+        {
+            return;
+        }
+
+        var currentNode = new TreeViewItem { Header = element.GetType().Name };
+        treeViewItem.Items.Add(currentNode);
+
+        // 获取当前元素的子元素
+        var elements = LogicalTreeHelper.GetChildren(dependencyObject);
+
+        foreach (var child in elements)
+        {
+            LoadLogicTree(currentNode, child);
+        }
+    }
+}

aeb783f0490db751474d8e12a7aeb27bb93bc7bb
shiloh <lixiaolei595@gmail.com>
2024-01-09 18:19:15 +0800

    feat(Template): DataTemplate example

---
+++ wpf/Template/Wpf.Template.DataTemplate/Wpf.Template.DataTemplate/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Wpf.Template.DataTemplate.ViewModel;
+
+namespace Wpf.Template.DataTemplate;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new MainViewModel();
+    }
+}
---
+++ wpf/Template/Wpf.Template.DataTemplate/Wpf.Template.DataTemplate/ViewModel/MainViewModel.cs
@@ -0,0 +1,83 @@
+﻿using Wpf.Template.DataTemplate.Model;
+
+namespace Wpf.Template.DataTemplate.ViewModel;
+
+/// <summary>
+/// 主窗体数据上下文
+/// </summary>
+public class MainViewModel : ObservableObject
+{
+    /// <summary>
+    /// backing field of <see cref="Person"/>
+    /// </summary>
+    private Person _person;
+
+    /// <summary>
+    /// 人员信息
+    /// </summary>
+    public Person Person
+    {
+        get => _person;
+        set
+        {
+            _person = value;
+            OnPropertyChanged();
+        }
+    }
+
+    /// <summary>
+    /// backing field of <see cref="Persons"/>
+    /// </summary>
+    private List<Person> _persons = [];
+
+    /// <summary>
+    /// 人员信息集合
+    /// </summary>
+    public List<Person> Persons
+    {
+        get => _persons;
+        set
+        {
+            _persons = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public MainViewModel()
+    {
+        _person = new Person
+        {
+            Name = "张三",
+            Age = 20,
+            Occupation = "程序员",
+            Money = 99999,
+            Address = "北京"
+        };
+        _persons.Add(_person);
+        
+        _persons.Add(new Person
+        {
+            Name = "李四",
+            Age = 21,
+            Occupation = "程序员",
+            Money = 99999,
+            Address = "北京"
+        });
+        _persons.Add(new Person
+        {
+            Name = "王五",
+            Age = 22,
+            Occupation = "程序员",
+            Money = 99999,
+            Address = "北京"
+        });
+        _persons.Add(new Person
+        {
+            Name = "赵六",
+            Age = 23,
+            Occupation = "程序员",
+            Money = 99999,
+            Address = "北京"
+        });
+    }
+}

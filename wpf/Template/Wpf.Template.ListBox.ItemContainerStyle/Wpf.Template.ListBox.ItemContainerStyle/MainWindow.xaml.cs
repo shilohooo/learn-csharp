4704613fe693c3f0eeda7c39f439b3f02a28fb1a
shiloh <lixiaolei595@gmail.com>
2024-01-10 19:22:01 +0800

    feat(Template): ListBox ItemContainerStyle example

---
+++ wpf/Template/Wpf.Template.ListBox.ItemContainerStyle/Wpf.Template.ListBox.ItemContainerStyle/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Wpf.Template.ListBox.ItemContainerStyle.ViewModel;
+
+namespace Wpf.Template.ListBox.ItemContainerStyle;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new MainViewModel();
+    }
+}

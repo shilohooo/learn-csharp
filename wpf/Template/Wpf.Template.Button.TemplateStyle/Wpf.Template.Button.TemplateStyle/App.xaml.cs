eac907ce11d65354caff42d10b27f515cb3d65fb
shiloh <lixiaolei595@gmail.com>
2024-01-10 20:07:22 +0800

    feat(Template): Button TemplateStyle example

---
+++ wpf/Template/Wpf.Template.Button.TemplateStyle/Wpf.Template.Button.TemplateStyle/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace Wpf.Template.Button.TemplateStyle;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}

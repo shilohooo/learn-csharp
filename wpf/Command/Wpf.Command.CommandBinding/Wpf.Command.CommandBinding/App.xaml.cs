30f75c3897b536b9eee4ecfbd902f08ff6ceae01
shiloh <lixiaolei595@gmail.com>
2024-01-11 20:27:06 +0800

    feat(Command): CommandBinding example

---
+++ wpf/Command/Wpf.Command.CommandBinding/Wpf.Command.CommandBinding/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Wpf.Command.CommandBinding
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ wpf/Command/Wpf.Command.CommandBinding/Wpf.Command.CommandBinding/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Wpf.Command.CommandBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("我是ALT+S");
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+    }
+}

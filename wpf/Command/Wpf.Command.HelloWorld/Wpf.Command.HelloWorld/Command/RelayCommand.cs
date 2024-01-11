505f7cedf760b4bc2b315b96b557244969e1842f
shiloh <lixiaolei595@gmail.com>
2024-01-11 12:14:01 +0800

    feat(Command): Command example

---
+++ wpf/Command/Wpf.Command.HelloWorld/Wpf.Command.HelloWorld/Command/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Input;
+
+namespace Wpf.Command.HelloWorld.Command;
+
+/// <summary>
+/// 自定义命令
+/// </summary>
+public class RelayCommand<T>(Action<T?> action) : ICommand
+{
+    /// <summary>
+    /// 命令执行时，要调用的委托
+    /// </summary>
+    private readonly Action<T?> _action = action;
+
+    /// <summary>
+    /// 判断命令是否可以执行，如果可以执行，返回 <code>true</code>，否则返回 <code>false</code>
+    /// </summary>
+    /// <param name="parameter">任务参数，可以为 <code>null</code></param>
+    /// <returns></returns>
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    /// <summary>
+    /// 如果命令可以执行，那么 WPF 会自动调用改方法，使用委托来执行业务逻辑
+    /// </summary>
+    /// <param name="parameter">任务参数</param>
+    public void Execute(object? parameter)
+    {
+        // 调用委托，执行具体的方法
+        _action?.Invoke((T?)parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+}
---
+++ wpf/Command/Wpf.Command.HelloWorld/Wpf.Command.HelloWorld/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Wpf.Command.HelloWorld.Command;
+
+namespace Wpf.Command.HelloWorld.ViewModel;
+
+public class MainViewModel
+{
+    /// <summary>
+    /// 命令属性
+    /// </summary>
+    public RelayCommand<object> OpenCommand { get; } = new(param =>
+    {
+        Console.WriteLine($"Command param type: {param?.GetType().Name}");
+        MessageBox.Show($"Hello WPF Command:) {param}");
+    });
+}

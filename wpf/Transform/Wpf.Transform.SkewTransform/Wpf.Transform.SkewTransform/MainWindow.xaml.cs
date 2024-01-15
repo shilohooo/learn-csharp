35867d87b1237d1f5830f5793bfab6b098ce2066
shiloh <lixiaolei595@gmail.com>
2024-01-15 21:55:47 +0800

    feat(Transform): SkewTransform example

---
+++ wpf/Transform/Wpf.Transform.SkewTransform/Wpf.Transform.SkewTransform/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace Wpf.Transform.SkewTransform;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}

ac22b9e92e44d48d7162c8f465166e58f559c877
shiloh <lixiaolei595@gmail.com>
2024-01-16 18:16:55 +0800

    feat(Brush): SolidColorBrush example

---
+++ wpf/Brush/SolidColorBrush/SolidColorBrush/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace SolidColorBrush;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        // Loaded += (_, _) =>
+        // {
+        //     System.Windows.Media.SolidColorBrush solidColorBrush = new()
+        //     {
+        //         Color = Colors.Green
+        //     };
+        //     // 从 Color 对象的 FromRgb 方法中得到颜色
+        //     solidColorBrush.Color = Color.FromRgb(0, 0x80, 0);
+        //     Grid.Background = solidColorBrush;
+        // };
+    }
+}

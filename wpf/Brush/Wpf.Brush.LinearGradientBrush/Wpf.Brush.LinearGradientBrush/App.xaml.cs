3138508c2fa7a20c3ffa94969aeac537f3f89fa6
shiloh <lixiaolei595@gmail.com>
2024-01-17 21:46:00 +0800

    feat(Brush): LinearGradientBrush example

---
+++ wpf/Brush/Wpf.Brush.LinearGradientBrush/Wpf.Brush.LinearGradientBrush/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace Wpf.Brush.LinearGradientBrush;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ wpf/Brush/Wpf.Brush.LinearGradientBrush/Wpf.Brush.LinearGradientBrush/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Wpf.Brush.LinearGradientBrush;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void MainWindow_OnMouseMove(object sender, MouseEventArgs e)
+    {
+        if (Grid.Background is not System.Windows.Media.LinearGradientBrush gridBrush)
+        {
+            return;
+        }
+
+        var gradientStop0 = gridBrush.GradientStops[0];
+        var gradientStop1 = gridBrush.GradientStops[1];
+        var point = e.GetPosition(this);
+        var offset = (point.X + point.Y) / (Width + Height);
+        gradientStop0.Offset = offset;
+        gradientStop1.Offset = 1 - offset;
+    }
+}

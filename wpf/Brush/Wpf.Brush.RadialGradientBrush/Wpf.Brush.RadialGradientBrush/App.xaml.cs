1d2082301cc6da3a03f4e470ed2c50ed14693c6a
shiloh <lixiaolei595@gmail.com>
2024-01-17 21:55:08 +0800

    feat(Brush): RadialGradientBrush example

---
+++ wpf/Brush/Wpf.Brush.RadialGradientBrush/Wpf.Brush.RadialGradientBrush/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows;
+
+namespace Wpf.Brush.RadialGradientBrush;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ wpf/Brush/Wpf.Brush.RadialGradientBrush/Wpf.Brush.RadialGradientBrush/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace Wpf.Brush.RadialGradientBrush;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Ellipse_OnMouseMove(object sender, MouseEventArgs e)
+    {
+        if (e.Source is not Ellipse ellipse)
+        {
+            return;
+        }
+
+        var point = e.GetPosition(ellipse);
+        var width = ellipse.Width;
+        var height = ellipse.Height;
+        if (ellipse.Fill is not System.Windows.Media.RadialGradientBrush brush)
+        {
+            return;
+        }
+
+        var x = point.X / width;
+        var y = point.Y / height;
+        brush.GradientOrigin = new Point(x, y);
+    }
+}

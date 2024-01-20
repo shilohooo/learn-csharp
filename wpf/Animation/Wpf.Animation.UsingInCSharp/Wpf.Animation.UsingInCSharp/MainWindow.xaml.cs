ce59b48cf9e5f5c5d885e17cb9b287a945644050
shiloh <lixiaolei595@gmail.com>
2024-01-20 23:31:15 +0800

    feat(Animation): Using Animation in CSharp code

---
+++ wpf/Animation/Wpf.Animation.UsingInCSharp/Wpf.Animation.UsingInCSharp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Wpf.Animation.UsingInCSharp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Grid_OnMouseUp(object sender, MouseButtonEventArgs e)
+    {
+        var point = e.GetPosition(Grid);
+        if (Ellipse.RenderTransform is not ScaleTransform scaleTransform)
+        {
+            return;
+        }
+
+        var doubleAnimation = new DoubleAnimation
+        {
+            To = (point.X + point.Y) / 200,
+            Duration = new TimeSpan(
+                0, 0, 0, 0, 250
+            )
+        };
+
+        // 通过动画修改ScaleX和ScaleY属性达到放大的效果
+        scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, doubleAnimation);
+        scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, doubleAnimation);
+    }
+}

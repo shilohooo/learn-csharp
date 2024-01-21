1064df32d4cc6e62407db6e78e25f661047e2de1
shiloh <lixiaolei595@gmail.com>
2024-01-21 13:11:03 +0800

    feat(Animation): PathAnimation example

---
+++ wpf/Animation/Wpf.Animation.Path/Wpf.Animation.Path/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Wpf.Animation.Path;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Canvas_OnMouseUp(object sender, MouseButtonEventArgs e)
+    {
+    }
+}

d83096e8e97475c32f37c72cf8a9f6e6b414a946
shiloh <lixiaolei595@gmail.com>
2024-01-22 12:27:28 +0800

    feat(Navigation): HelloWorld

---
+++ wpf/Navigation/Wpf.Navigation.HelloWorld/Wpf.Navigation.HelloWorld/PageDemo.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Wpf.Navigation.HelloWorld;
+
+public partial class PageDemo : Page
+{
+    public PageDemo()
+    {
+        InitializeComponent();
+    }
+}

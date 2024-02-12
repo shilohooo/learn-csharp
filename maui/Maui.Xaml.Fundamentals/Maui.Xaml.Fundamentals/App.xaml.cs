dd2e4436d19ef0e43de0b99dc1b9aa56ed9f2f84
shiloh <lixiaolei595@gmail.com>
2024-02-12 22:04:05 +0800

    feat(MAUI): XAML hello world

---
+++ maui/Maui.Xaml.Fundamentals/Maui.Xaml.Fundamentals/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui.Xaml.Fundamentals;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ maui/Maui.Xaml.Fundamentals/Maui.Xaml.Fundamentals/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Maui.Xaml.Fundamentals;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}

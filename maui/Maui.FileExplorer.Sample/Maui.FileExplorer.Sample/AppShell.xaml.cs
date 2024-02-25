fee24bc0699de6ef20947e69cd53a0aac3518697
shiloh <lixiaolei595@gmail.com>
2024-02-25 15:40:42 +0800

    feat(MAUI): MAUI File Explorer Sample

---
+++ maui/Maui.FileExplorer.Sample/Maui.FileExplorer.Sample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.FileExplorer.Sample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ maui/Maui.FileExplorer.Sample/Maui.FileExplorer.Sample/Views/DrivePage.xaml.cs
@@ -0,0 +1,12 @@
+namespace Maui.FileExplorer.Sample.Views;
+
+/// <summary>
+/// 驱动器信息页面交互逻辑
+/// </summary>
+public partial class DrivePage : ContentPage
+{
+    public DrivePage()
+    {
+        InitializeComponent();
+    }
+}

c53ca9f0ae8d4fcd9ddeaabbf7abcf9504096fcf
shiloh <lixiaolei595@gmail.com>
2024-03-01 07:25:32 +0800

    feat(ASP.NET): Web APIs example

---
+++ aspnet/TodoApi/TodoApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Models;
+
+/// <summary>
+/// 实体
+/// </summary>
+public class TodoItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsComplete { get; set; }
+}

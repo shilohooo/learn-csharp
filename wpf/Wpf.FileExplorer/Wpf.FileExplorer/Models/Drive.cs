6d5cb3919e2081b49e55160084a6702cb54f5a9d
shiloh <lixiaolei595@gmail.com>
2024-03-13 14:20:20 +0800

    feat(WPF): file explorer example

---
+++ wpf/Wpf.FileExplorer/Wpf.FileExplorer/Models/Drive.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Wpf.FileExplorer.Models;
+
+/// <summary>
+/// 驱动器信息
+/// </summary>
+public partial class Drive : ObservableObject
+{
+    /// <summary>
+    /// 驱动器标签，如：系统、软件
+    /// </summary>
+    [ObservableProperty] private string? _label;
+
+    /// <summary>
+    /// 驱动器名称，如：C:\、D:\
+    /// </summary>
+    [ObservableProperty] private string? _name;
+
+    /// <summary>
+    /// 总空间，单位：GB
+    /// </summary>
+    [ObservableProperty] private long _totalSpace;
+
+    /// <summary>
+    /// 可用空间，单位：GB
+    /// </summary>
+    [ObservableProperty] private long _availableSpace;
+
+    /// <summary>
+    /// 根目录绝对路径
+    /// </summary>
+    [ObservableProperty] private string? _rootDirFullPath;
+
+    /// <summary>
+    /// 加载驱动器信息
+    /// </summary>
+    /// <returns></returns>
+    public static IEnumerable<Drive> LoadAllDrives()
+    {
+        return DriveInfo.GetDrives().Select(driveInfo => new Drive
+        {
+            Label = driveInfo.VolumeLabel,
+            Name = $"({driveInfo.Name.Replace("\\", "")})",
+            TotalSpace = driveInfo.TotalSize / 1024 / 1024 / 1024,
+            AvailableSpace = driveInfo.AvailableFreeSpace / 1024 / 1024 / 1024,
+            RootDirFullPath = driveInfo.RootDirectory.FullName
+        });
+    }
+}
---
+++ wpf/Wpf.FileExplorer/Wpf.FileExplorer/ViewModels/DriveViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Wpf.FileExplorer.Models;
+
+namespace Wpf.FileExplorer.ViewModels;
+
+/// <summary>
+/// 驱动器信息视图模型
+/// </summary>
+public partial class DriveViewModel : ObservableObject
+{
+    /// <summary>
+    /// 驱动器信息列表
+    /// </summary>
+    [ObservableProperty] private ObservableCollection<Drive>? _drives;
+
+    /// <summary>
+    /// 驱动器选择命令
+    /// </summary>
+    public ICommand SelectDriveCommand { get; set; }
+
+    public DriveViewModel()
+    {
+        _drives = new ObservableCollection<Drive>(Drive.LoadAllDrives());
+        SelectDriveCommand = new RelayCommand<Drive>(SelectDrive);
+    }
+
+    /// <summary>
+    /// 选择驱动器
+    /// </summary>
+    /// <param name="drive">当前选择的驱动器信息</param>
+    private void SelectDrive(Drive? drive)
+    {
+        if (drive == null)
+        {
+            return;
+        }
+
+        MessageBox.Show(
+            $"当前选择的驱动器信息 - 标签：{drive.Label}，名称：{drive.Name}，总空间：{drive.TotalSpace}GB，可用空间：{drive.AvailableSpace}GB，根目录绝对路径：{drive.RootDirFullPath}");
+    }
+}

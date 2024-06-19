a6d5cc006afa9616a38eeea037091abc341088cd
shiloh <lixiaolei595@gmail.com>
2024-06-19 17:45:46 +0800

    feat: read xml content example

---
+++ XmlParse.Example/XmlParse.Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml;
+
+namespace XmlParse.Example;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 读取 D 盘下面的 test.xml 文件的内容
+        var xml = File.ReadAllText(@"D:\test.xml");
+        var xmlDocument = new XmlDocument();
+        xmlDocument.LoadXml(xml);
+        var rootEl = xmlDocument.DocumentElement;
+        Console.WriteLine(rootEl?.Name);
+    }
+}

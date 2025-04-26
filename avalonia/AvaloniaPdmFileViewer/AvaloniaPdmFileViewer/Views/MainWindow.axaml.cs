96a2d653607f330365e5a655470522c9ad8ad9ea
shiloh <lixiaolei595@gmail.com>
2025-04-26 12:03:59 +0800

    feat: using Material.Avalonia theme

--- avalonia/AvaloniaPdmFileViewer/AvaloniaPdmFileViewer/Views/MainWindow.axaml.cs
+++ avalonia/AvaloniaPdmFileViewer/AvaloniaPdmFileViewer/Views/MainWindow.axaml.cs
@@ -8,28 +8,4 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
-
-    // private void Button_OnClicked(object? sender, RoutedEventArgs e)
-    // {
-    //     Debug.WriteLine($"BUtton Click! Celsius={Celsius.Text}");
-    //     CelsiusToFahrenheit();
-    // }
-    //
-    // private void Celsius_OnTextChanged(object? sender, TextChangedEventArgs e)
-    // {
-    //     Debug.WriteLine($"Celsius change to {Celsius.Text}");
-    //     CelsiusToFahrenheit();
-    // }
-    //
-    // private void CelsiusToFahrenheit()
-    // {
-    //     if (double.TryParse(Celsius.Text, out var c))
-    //     {
-    //         Fahrenheit.Text = (c * (9d / 5d) + 32).ToString("0.0");
-    //         return;
-    //     }
-    //
-    //     Celsius.Text = "0";
-    //     Fahrenheit.Text = "0";
-    // }
 }

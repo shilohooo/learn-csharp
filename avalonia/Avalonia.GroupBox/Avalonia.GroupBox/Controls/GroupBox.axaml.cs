84683ee14a03c58c32bb908942ca92a3bac3a760
shiloh <lixiaolei595@gmail.com>
2025-05-07 17:51:45 +0800

    feat(avalonia): GroupBox example

---
+++ avalonia/Avalonia.GroupBox/Avalonia.GroupBox/Controls/GroupBox.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia.Controls;
+
+namespace Avalonia.GroupBox.Controls;
+
+public class GroupBox : ContentControl
+{
+    #region Styled Properties
+
+    // 自定义样式属性，可以绑定到控件的样式上 Text={TemplateBinding Header}
+
+    public static readonly StyledProperty<string> HeaderProperty =
+        AvaloniaProperty.Register<GroupBox, string>(nameof(Header), "Header");
+
+    /// <summary>
+    ///     标题
+    /// </summary>
+    public string Header
+    {
+        get => GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+
+    #endregion
+}

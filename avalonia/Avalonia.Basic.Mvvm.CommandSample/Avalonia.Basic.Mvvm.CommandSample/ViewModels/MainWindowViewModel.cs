3284d9af2a31260d16e618b80c8fe55db554e1b5
shiloh <lixiaolei595@gmail.com>
2025-05-08 18:05:11 +0800

    feat(avalonia): add command sample

---
+++ avalonia/Avalonia.Basic.Mvvm.CommandSample/Avalonia.Basic.Mvvm.CommandSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Avalonia.Basic.Mvvm.CommandSample.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+    
+    public CommandsViewModel CommandsViewModel { get; } = new();
+}

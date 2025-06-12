b4ecb5b0a117e9545465d5d14fa99ae55f7463c5
shiloh <lixiaolei595@gmail.com>
2025-06-12 17:13:10 +0800

    feat(avalonia): 主窗口改为无边框样式

--- avalonia/Avalonia.PageNavigationSample/Avalonia.PageNavigationSample/Views/MainWindow.axaml.cs
+++ avalonia/Avalonia.PageNavigationSample/Avalonia.PageNavigationSample/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace Avalonia.PageNavigationSample.Views;
 
@@ -7,5 +8,12 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        // 实现无边框窗口拖动
+        PointerPressed += (_, e) =>
+        {
+            if (e.Pointer.Type is not PointerType.Mouse) return;
+
+            BeginMoveDrag(e);
+        };
     }
 }
